38616babcc6ad4ed05b1dd8e209a718d8d9a7bb6
Misbah-Saeed <saeedmisbah2008@gmail.com>
2024-12-16 08:37:19 -0800

    Add files via upload

---
+++ src/app/Component/HeroSection.tsx
@@ -0,0 +1,18 @@
+export default function HeroSection (){
+      return (
+        <div>
+    <div className="flex justify-center gap-5 h-auto w-full p-4 mt-8">
+    <div className="h-80 w-[450px] bg-pink-300 rounded-md shadow-2xl"></div>
+    <div className="h-80 w-[450px] bg-red-500 rounded-md shadow-2xl"></div>
+    <div className="h-80 w-[450px] bg-yellow-500 rounded-md shadow-2xl"></div>
+    </div>
+    
+    <div className=" flex justify-center gap-2 h-auto w-full p-4 mt-8">
+    <div className="h-80 w-[450px] bg-green-500 rounded-md shadow-2xl"></div>
+    <div className="h-80 w-[450px] bg-orange-500 rounded-md shadow-2xl"></div>
+    </div>
+    </div>
+      )
+    }
+    
+   
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./Component/Header";
+import HeroSection from "./Component/HeroSection";
+import Footer from "./Component/Footer"
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <HeroSection/>
+      <Footer/>
+    </div>
+  )
+}
